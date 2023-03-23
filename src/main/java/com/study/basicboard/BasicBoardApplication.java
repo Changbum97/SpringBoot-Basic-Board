a9dbf664e419e812675e35845c35567d64215b24
Changbum97 <chb2005@naver.com>
2023-03-23 17:19:03 +0900

    first commit

---
+++ src/main/java/com/study/basicboard/BasicBoardApplication.java
@@ -0,0 +1,13 @@
+package com.study.basicboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/basicboard/BasicBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.basicboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
