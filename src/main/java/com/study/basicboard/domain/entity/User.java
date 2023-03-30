1b48c5953c0efbe008b0b63f6c0879ddb93387ec
Changbum97 <chb2005@naver.com>
2023-03-30 21:29:01 +0900

    FEAT: User Entity 추가

--- src/main/java/com/study/basicboard/domain/entity/User.java
+++ src/main/java/com/study/basicboard/domain/entity/User.java
@@ -1,6 +1,10 @@
 package com.study.basicboard.domain.entity;
 
 import com.study.basicboard.domain.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +12,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
 public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
