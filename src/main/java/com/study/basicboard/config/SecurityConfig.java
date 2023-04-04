eecf7321dda9782f28c14820c8ed7cfc10bf8ff5
Changbum97 <chb2005@naver.com>
2023-04-04 14:52:06 +0900

    FEAT: Security Config 추가

---
+++ src/main/java/com/study/basicboard/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.study.basicboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf().disable()
+                .cors().and()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and().build();
+    }
+}
