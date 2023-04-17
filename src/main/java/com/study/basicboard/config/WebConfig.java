9ee5d619dbfeb70c0ecd0cab8b6c2d7cb9c72de1
Changbum97 <chb2005@naver.com>
2023-04-17 20:46:02 +0900

    FIX: 파일 업로드 에러 수정

--- src/main/java/com/study/basicboard/config/WebConfig.java
+++ src/main/java/com/study/basicboard/config/WebConfig.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://43.200.173.234:8084")
-                .allowedMethods("GET", "POST");
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
