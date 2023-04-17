b6e6907a75a71ed7691cdc16bee103aded40b3e7
Changbum97 <chb2005@naver.com>
2023-04-17 16:48:59 +0900

    FEAT: Cors 에러 해결

---
+++ src/main/java/com/study/basicboard/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.study.basicboard.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://43.200.173.234:8084")
+                .allowedMethods("GET", "POST");
+    }
+}
