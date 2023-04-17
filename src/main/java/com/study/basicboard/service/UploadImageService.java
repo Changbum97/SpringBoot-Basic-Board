adb442d0471f5e4fa0b523192929cc51bd64b5f4
Changbum97 <chb2005@naver.com>
2023-04-17 19:19:04 +0900

    FIX: 파일 업로드 에러 수정

--- src/main/java/com/study/basicboard/service/UploadImageService.java
+++ src/main/java/com/study/basicboard/service/UploadImageService.java
@@ -45,17 +45,20 @@ public class UploadImageService {
         String savedFilename = UUID.randomUUID() + "." + extractExt(originalFilename);
 
         // 파일 저장
-        //File newFile = new File(getFullPath(savedFilename));
         System.out.println("============2========");
-        //multipartFile.transferTo(newFile);
-        multipartFile.transferTo( Paths.get(getFullPath(savedFilename)) );
-        System.out.println("============3========");
+        try {
+            multipartFile.transferTo(new File(getFullPath(savedFilename)));
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            System.out.println("============3========");
 
-        return uploadImageRepository.save(UploadImage.builder()
-                .originalFilename(originalFilename)
-                .savedFilename(savedFilename)
-                .board(board)
-                .build());
+            return uploadImageRepository.save(UploadImage.builder()
+                    .originalFilename(originalFilename)
+                    .savedFilename(savedFilename)
+                    .board(board)
+                    .build());
+        }
     }
 
     @Transactional
