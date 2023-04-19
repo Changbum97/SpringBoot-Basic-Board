142f99dc0b2485509e4f9fd466e0a428dc73644f
Changbum97 <chb2005@naver.com>
2023-04-19 15:03:08 +0900

    FIX: Spring Project Timezone 설정

--- src/main/java/com/study/basicboard/BasicBoardApplication.java
+++ src/main/java/com/study/basicboard/BasicBoardApplication.java
@@ -3,9 +3,16 @@ package com.study.basicboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BasicBoardApplication {
 
+    @PostConstruct
+    public void setTimeZone(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(BasicBoardApplication.class, args);
     }
